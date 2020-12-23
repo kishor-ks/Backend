482784d36ac9d44d7403cefad9e6f7e334fa377b
kishor-ks <76027741+kishor-ks@users.noreply.github.com>
2020-12-23 17:10:38 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const app = require("./app");
 
 const server = http.createServer(app);
 
-server.listen(3000, ()=>{
+server.listen( process.env.PORT || 3000, ()=>{
   console.log("Check port Number 3000")
 });
